0e5b90b0df185ef7edc2695752f095cc172d4fe6
alis13nya <166594142+alis13nya@users.noreply.github.com>
2024-10-13 14:55:15 +0300

    Add files via upload

---
+++ alis.kr1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введите слово КАПСОМ на русском: ");
+string word = Console.ReadLine();
+Console.WriteLine(shifr(word));
+
+static string shifr (string word)
+{
+    int index;
+    string result="";
+    char[] letters = new char[] { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я' };
+    char[] letters2 = new char[] { 'Я', 'Ю', 'Э', 'Ь', 'Ы', 'Ъ', 'Щ', 'Ш', 'Ч', 'Ц', 'Х', 'Ф', 'У', 'Т', 'С', 'Р', 'П', 'О', 'Н', 'М', 'Л', 'К', 'Й', 'И', 'З', 'Ж', 'Ё', 'Е', 'Д', 'Г', 'В', 'Б', 'А' };
+
+    foreach (char letter in word)
+    {
+        index = Array.IndexOf(letters, letter);
+        result = result + letters2[index];
+    }
+    return result;
+}
+
