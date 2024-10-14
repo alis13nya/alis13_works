9d49eaed1d1fde9be984d3726a39391c8c2a53b1
alis13nya <166594142+alis13nya@users.noreply.github.com>
2024-10-14 22:00:43 +0300

    Add files via upload

---
+++ кз_2.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Math;
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+for (; n!= 1; i++)
+{
+    if ((int)n % 2 == 0)
+    {
+        n = n / 2;
+    }
+    else
+    {
+        n = 3 * n + 1;
+    }
+    Console.WriteLine(n);
+}
+Console.WriteLine($"число замен {i}");
